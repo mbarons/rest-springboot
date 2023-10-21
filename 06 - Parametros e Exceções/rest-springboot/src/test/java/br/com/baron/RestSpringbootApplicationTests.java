86bc169810e4014ef4444a8260c967ae7afa57b1
mbaron <57764271+mbarons@users.noreply.github.com>
2023-10-21 12:41:22 -0300

    criação da seção 6

---
+++ 06 - Parametros e Exceções/rest-springboot/src/test/java/br/com/baron/RestSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.baron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
